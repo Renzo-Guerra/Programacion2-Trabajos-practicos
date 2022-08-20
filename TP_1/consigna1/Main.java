4971e509481280524506630bf2bea636e9204f64
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-08-19 22:31:28 -0300

    ✨ FEATURE: Add archivo main de la consigna 1
    - Se creó un objeto por cada posible contructor, y se
    mostraron los datos de todos los objetos a traves
    de un procedimiento

---
+++ TP_1/consigna1/Main.java
@@ -0,0 +1,41 @@
+package TP_1.consigna1;
+
+public class Main {
+  public static void main(String[] args) {
+    int dni_ingresar = 24504877;
+    String nombre = "Franco";
+    String fecha_nacimiento = "3/5/1982";
+    char sexo = 'm';
+    int altura = 180;
+    int peso = 89;
+
+    Persona sujeto_1 = new Persona(dni_ingresar);
+    Persona sujeto_2 = new Persona(dni_ingresar, nombre);
+    Persona sujeto_3 = new Persona(dni_ingresar, nombre, fecha_nacimiento);
+    Persona sujeto_4 = new Persona(dni_ingresar, nombre, fecha_nacimiento, sexo);
+    Persona sujeto_5 = new Persona(dni_ingresar, nombre, fecha_nacimiento, sexo, altura);
+    Persona sujeto_6 = new Persona(dni_ingresar, nombre, fecha_nacimiento, sexo, altura, peso);
+    
+    System.out.println("\nInformacion del sujeto_1: ");
+    mostrarDatos(sujeto_1);
+    System.out.println("\nInformacion del sujeto_2: ");
+    mostrarDatos(sujeto_2);
+    System.out.println("\nInformacion del sujeto_3: ");
+    mostrarDatos(sujeto_3);
+    System.out.println("\nInformacion del sujeto_4: ");
+    mostrarDatos(sujeto_4);
+    System.out.println("\nInformacion del sujeto_5: ");
+    mostrarDatos(sujeto_5);
+    System.out.println("\nInformacion del sujeto_6: ");
+    mostrarDatos(sujeto_6);
+  }
+
+  public static void mostrarDatos(Persona sujeto){
+    System.out.println("DNI: " + sujeto.getDNI());
+    System.out.println("Nombre: " + sujeto.getNombre());
+    System.out.println("Fecha de nacimiento: " + sujeto.getFechaNacimiento());
+    System.out.println("Sexo: " + sujeto.getSexo());
+    System.out.println("Altura: " + sujeto.getAltura());
+    System.out.println("Peso: " + sujeto.getPeso());
+  }
+}
