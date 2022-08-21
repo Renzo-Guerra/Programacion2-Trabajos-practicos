c460447c5e4b7ffaaeb1c153e28c7236954a78c9
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-08-21 15:58:25 -0300

    ✨ FEATURE: Add getters /setters de Rectangulo
    - Los setters son de accesibilidad 'private' debido a que si se
    pudiese settear un punto solo sin consideracion de los demas, dejaria
    de ser un rectangulo. Debido a eso, al momento de trasladar un
    rectangulo, se moveran todos los puntos 'i' posiciones en el eje x,
    y se moveran 'j' posiciones en el eje y.

--- TP_1/consigna3/Rectangulo.java
+++ TP_1/consigna3/Rectangulo.java
@@ -21,6 +21,27 @@ public class Rectangulo {
     setPunto_c(c);
     setPunto_d(d);
   }
+  
+  // Setters
+  private void setPunto_a(PuntoGeometrico a){
+    this.punto_a = a;
+  }
+  private void setPunto_b(PuntoGeometrico b){
+    this.punto_b = b;
+  }
+  private void setPunto_c(PuntoGeometrico c){
+    this.punto_c = c;
+  }
+  private void setPunto_d(PuntoGeometrico d){
+    this.punto_d = d;
+  }
+
+  // Getters
+  public PuntoGeometrico getPunto_a(){return this.punto_a;}
+  public PuntoGeometrico getPunto_b(){return this.punto_b;}
+  public PuntoGeometrico getPunto_c(){return this.punto_c;}
+  public PuntoGeometrico getPunto_d(){return this.punto_d;}
+
   // Valida que los 4 vertices dados puedan conformar un rectangulo.
   public static boolean sonVerticesValidos(PuntoGeometrico a, PuntoGeometrico b, PuntoGeometrico c, PuntoGeometrico d){
     /*
