c311b4f79aa26174c9cb964eb8f44d17177347ea
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-08-21 12:42:27 -0300

    🎉 CREATE: Add consigna3/ con su clase
    - Se creo una carpeta para la consigna 3,
    la cual posee la clase PuntoGeometrico.

---
+++ TP_1/consigna3/PuntoGeometrico.java
@@ -0,0 +1,9 @@
+package TP_1.consigna3;
+
+// Libreria que nos permitira utilizar metodos para calculos.
+import java.math.*;
+
+public class PuntoGeometrico {
+  private float coordenada_x;
+  private float coordenada_y;
+}
