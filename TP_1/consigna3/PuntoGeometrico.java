3cb54c4c0f4b5db932c0df353037c5934c887ef7
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-08-21 13:23:50 -0300

    ✨ FEATURE: Add metodo 'calcularDistanciaEuclidea'
    - Dado un punto es posible establecer la distancia euclídea con otro
    punto acorde a la siguiente formula:
    distancia^2= (X1 – X2)^2 + (Y1 – Y2)^2

--- TP_1/consigna3/PuntoGeometrico.java
+++ TP_1/consigna3/PuntoGeometrico.java
@@ -1,7 +1,5 @@
 package TP_1.consigna3;
 
-// Libreria que nos permitira utilizar metodos para calculos.
-import java.math.*;
 // Permite aniadir un formato decimal prestablecido (devuelve un tipo String, por eso se usa Float.parseFloat())
 import java.text.DecimalFormat;
 
@@ -33,9 +31,26 @@ public class PuntoGeometrico {
   public float getCoordenadaY(){return this.coordenada_y;}
 
   // Metodos
-
   public void desplazarPuntoEnElPlano(float x, float y){
     setCoordenadaX(x);
     setCoordenadaY(y);
+  }
+
+  public double calcularDistanciaEuclidea(Double nuevo_punto_x, Double nuevo_punto_y){
+    final DecimalFormat formato1 = new DecimalFormat("#.00");
+    final double coordenada_x = getCoordenadaX();
+    final double coordenada_y = getCoordenadaY();
+    double distancia;
+    /*
+     * - Se les introduce un formato decimal prestablecido.
+     * - Se utilizan datos de tipo "double" para poder utilizar el metodo "pow" de la clase Math
+    */
+    nuevo_punto_x = Double.parseDouble(formato1.format(nuevo_punto_x));
+    nuevo_punto_y = Double.parseDouble(formato1.format(nuevo_punto_y));
+    // Calculamos la distancia
+    distancia = Math.sqrt((Math.pow((coordenada_x - nuevo_punto_x), 2)) + (Math.pow((coordenada_y - nuevo_punto_y), 2)));
+    
+    // Devolvemos la distancia una vez aplicado el formato prestablecido
+    return Double.parseDouble(formato1.format(distancia));
   }
 }
