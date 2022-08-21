24dd3382a5b143a0490e85b48304a541e9d7bbe5
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-08-21 12:57:13 -0300

    ✨ FEATURE: Add setters / getters y libreria
    - Se agregaron los metodos setters y getters.
    - Se agrego la libreria 'java.text.DecimalFormat';

--- TP_1/consigna3/PuntoGeometrico.java
+++ TP_1/consigna3/PuntoGeometrico.java
@@ -2,6 +2,8 @@ package TP_1.consigna3;
 
 // Libreria que nos permitira utilizar metodos para calculos.
 import java.math.*;
+// Permite aniadir un formato decimal prestablecido (devuelve un tipo String, por eso se usa Float.parseFloat())
+import java.text.DecimalFormat;
 
 public class PuntoGeometrico {
   private float coordenada_x;
@@ -14,4 +16,20 @@ public class PuntoGeometrico {
     setCoordenadaX(x);
     setCoordenadaY(y);
   }
+
+  // Setters
+  public void setCoordenadaX(float x){
+    final DecimalFormat formato1 = new DecimalFormat("#.00");
+    this.coordenada_x = Float.parseFloat(formato1.format(x));
+  }
+
+  public void setCoordenadaY(float y){
+    final DecimalFormat formato1 = new DecimalFormat("#.00");
+    this.coordenada_y = Float.parseFloat(formato1.format(y));
+  }
+  
+  // Getters
+  public float getCoordenadaX(){return this.coordenada_x;}
+  public float getCoordenadaY(){return this.coordenada_y;}
+
 }
