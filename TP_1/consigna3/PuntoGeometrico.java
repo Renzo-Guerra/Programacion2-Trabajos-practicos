65ee172848f8cdd53158bf00c46d6ad8a97a3a72
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-08-21 12:50:21 -0300

    ✨ FEATURE: Add  contructores

--- TP_1/consigna3/PuntoGeometrico.java
+++ TP_1/consigna3/PuntoGeometrico.java
@@ -6,4 +6,12 @@ import java.math.*;
 public class PuntoGeometrico {
   private float coordenada_x;
   private float coordenada_y;
+
+  public PuntoGeometrico(){
+    this(0, 0);
+  }
+  public PuntoGeometrico(float x, float y){
+    setCoordenadaX(x);
+    setCoordenadaY(y);
+  }
 }
