6c78ae50430ed04399c8bf2ef0cb85f8b0acf086
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-09-22 11:02:36 -0300

    ⚡TESTING: Testing  PilaVector

--- TP_5/consigna1/Main.java
+++ TP_5/consigna1/Main.java
@@ -6,7 +6,7 @@ public class Main{
   public static void main(String[] args) {
     ArrayList<Object> testing = new ArrayList<>();
     PilaArrayList pila = new PilaArrayList();
-
+    System.out.println("Testeando PilaArrayList: ");
     System.out.println(pila.top());
     System.out.println(pila.pop());
     pila.push("Primer objeto");
@@ -22,5 +22,25 @@ public class Main{
     testing.clear();
     testing = pila.reverse();
     System.out.println(testing);
+
+
+    PilaVector pila2 = new PilaVector(4);
+    // Creamos una PilaVector para testear (de la misma capacidad que pila2)
+    PilaVector testing2 = new PilaVector(pila2.maxCapacidad());
+    System.out.println("\n\nTesteando PilaVector: ");
+    System.out.println(pila2.top());
+    System.out.println(pila2.pop());
+    pila2.push("Primer objeto");
+    pila2.push("Segundo objeto");
+    pila2.push(3);
+    pila2.push(4.00);
+    System.out.println("El tope es: " + pila2.top());
+    System.out.println("El objeto que se saco de la pila es: " + pila2.pop());
+    System.out.println("El tope es: " + pila2.top());
+    System.out.println("El tamaño de la pila es de: " + pila2.size());
+    testing2 = pila2.copy();
+    testing2.verElementos(); 
+    testing2 = pila2.reverse();
+    testing2.verElementos(); 
   }
 }
