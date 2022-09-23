d51c2eb0cb913eafdd42647825521afcec1e210e
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-09-22 22:37:20 -0300

    📚COMMENTS: Commented my interpretation
    - Notas mias previas a empezar a codear.

---
+++ TP_5/consigna3/Main.java
@@ -0,0 +1,28 @@
+package consigna3;
+
+/*
+ * Notas: 
+ * Hay 3 tipos de cereales: 
+ *  - Granos de cosecha gruesa.
+ *  - Granos de cosecha fina.
+ *  - Pasturas
+ * 
+ *  - Supongo que cada tipo de cereal tiene el mismo conjunto de minerales.
+ *    ej: Tanto "girasol" como "maiz" de tipo "cosecha gruesa" tienen los mismos minerales.
+ * 
+ *  - Es por eso que tendré que crear 1 clase para cada tipo de cosecha, cosa de definir 
+ *    en la instanciacion de cada tipo de cereal los minerales que este posee. 
+ *  
+ *  - Para que un cereal pueda ser plantado en un lote, el lote debe 
+ *    contener TODOS los minerales del cereal en cuestion.
+ *  - Las pasturas ademas, no pueden sembrarse en lotes menores a una 
+ *    superficie de 50 hectareas.
+ *  
+ *  - Tipos de lotes: 
+ *    - "especiales": "cuando contienen ciertos minerales de interés primario para la cooperativa"
+ *    - "comunes": "cuando contienen sólo minerales secundarios en la composición de la tierra"
+ */
+
+public class Main {
+  
+}
